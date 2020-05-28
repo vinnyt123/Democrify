854d7363161a775dd2523fcee69da8cb963b072c
vinnyt123 <vinny.tunnell@gmail.com>
2020-05-29 01:53:44 +1200

    fixed path issue

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ app.use('/api', apiRoutes);
 // Serve static assets if in production
 if (process.env.NODE_ENV === 'production') {
     // Set static folder
-    app.use(express.static('client/build'));
+    app.use(express.static('build'));
 
     app.get('*', (req, res) => {
         res.sendFile(path.resolve(__dirname, 'build', 'index.html'));
