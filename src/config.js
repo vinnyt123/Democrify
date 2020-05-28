3e7d0ae49577386720c126d36e62ca31543ea0aa
vinnyt123 <vinny.tunnell@gmail.com>
2020-05-29 00:19:40 +1200

    changed client ID and redirect URI

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
-export const clientId = 'a96316268d14415bb4cab791c0940004';
-export const redirectUri = 'http://localhost:3000/callback';
+export const clientId = 'c5732e95906f432bafee3dddbed2e4dc';
+export const redirectUri = 'https://blooming-beach-39759.herokuapp.com/callback';
 export const scopes = [
     "user-top-read",
     "user-read-currently-playing",
