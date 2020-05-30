da6310f5755f51ee547545561b63a9d3c3a569b5
vinnyt123 <vinny.tunnell@gmail.com>
2020-05-30 18:17:31 +1200

    changed redirect for new url

--- src/config.js
+++ src/config.js
@@ -1,7 +1,8 @@
 export const clientId = 'c5732e95906f432bafee3dddbed2e4dc';
-export const redirectUri = 'https://blooming-beach-39759.herokuapp.com/callback';
+//export const redirectUri = 'https://blooming-beach-39759.herokuapp.com/callback';
 // export const redirectUri = 'http://democrify.me/callback';
 // export const redirectUri = 'http://localhost:3000/callback';
+export const redirectUri = 'https://democrify-party.herokuapp.com/callback';
 export const scopes = [
     "user-top-read",
     "user-read-currently-playing",
